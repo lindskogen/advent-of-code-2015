0d43e89bba9744771fa3ba2fe86ab2d34e77be6b
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-12-11 01:52:34 +0100

    10 was pretty easy

---
+++ src/10/lookAndSay.js
@@ -0,0 +1,33 @@
+let expect = require('expect');
+
+const lookAndSay = (str) => {
+
+    let res = '';
+    let curr;
+    let currCount = 0;
+
+    for (var i = 0; i <= str.length; i++) {
+        if (curr === str.charAt(i)) {
+            currCount++;
+        } else {
+            if (currCount > 0) {
+                res += currCount + curr;
+            }
+            currCount = 1;
+            curr = str.charAt(i);
+        }
+    }
+
+    return res;
+};
+
+const repeatLookAndSay = (seed, times) => {
+    for (var i = times; i > 0; i--) {
+        seed = lookAndSay(seed);
+    }
+    return seed;
+};
+
+let result = repeatLookAndSay('3113322113', 40);
+
+console.log('result', result, result.length);
